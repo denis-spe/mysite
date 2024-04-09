70f7f86661aa67ec920f68c684aa707298e168e8
denis-spe <denisbrian07@gmail.com>
2024-04-09 19:57:57 +0200

    Add a form in detail.html

--- polls/urls.py
+++ polls/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from . import views
 
+app_name = "polls"
+
 urlpatterns = [
         # ex: /polls/
         path("", view=views.index, name="index"),
--- polls/views.py
+++ polls/views.py
@@ -1,7 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
 from .models import Question
 from django.template import loader
+from django.http import Http404
 
 # Create your views here.
 def index(request):
@@ -19,7 +20,20 @@ def index(request):
 
 
 def detail(request, question_id):
-    return HttpResponse("You're looking at question %s." % question_id)
+    """
+    try:
+        question = Question.objects.get(
+                pk=question_id)
+    except Question.DoesNotExist:
+        raise Http404("Question does not exist")
+    """
+    question = get_object_or_404(
+            Question, pk=question_id)
+
+    return render(
+            request,
+            "polls/detail.html", 
+            {"question": question})
 
 
 def results(request, question_id):
